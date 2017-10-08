79a1322873ca875cee35f7cbd5c52d2694894a17
re-fort <jeffpko@gmail.com>
2017-10-09 02:40:42 +0900

    :+1: No need specifying sass(scss) loader
    It's configured by vue-loader default

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -18,10 +18,6 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          },
           esModule: false,
         }
       },
